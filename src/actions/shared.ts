eedd3c4a7381239d3d71ae865303d138762b1f31
Jan T. Sott <git@idleberg.com>
2023-10-25 23:42:59 +0200

    add types

--- src/actions/shared.ts
+++ src/actions/shared.ts
@@ -3,14 +3,21 @@ import { convertPreset } from '@visbot/webvsc';
 import { basename } from "node:path";
 import { readFile, stat } from 'node:fs/promises';
 
-export const defaultOptions = {
+type Options = {
+	indent: number,
+	debug: boolean,
+	summary: boolean,
+	watch: boolean
+}
+
+export const defaultOptions: Options = {
 	indent: 2,
 	debug: false,
 	summary: false,
 	watch: false
 }
 
-export async function convertFile(avsFile: string, options = defaultOptions) {
+export async function convertFile(avsFile: string, options: Options = defaultOptions) {
 	const presetName = basename(avsFile, '.avs');
 	const avsBuffer = await readFile(avsFile);
 	const modifiedDate = ((await stat(avsFile)).mtime || new Date()).toISOString();
